0578799d72aa1022b586152ea6e1eb23e15cf5df
Jaime0Ivan <jaimeivan555@gmail.com>
2024-10-30 20:06:40 -0600

    configuracion del entorno 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hola Mundo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hola Mundo App'),
-        ),
-        body: const Center(
-          child: Text(
-            '¡Hola Mundo!',
-            style: TextStyle(fontSize: 24),
-          ),
+      home: const CounterPage(), // Cambiamos a CounterPage para manejar el contador
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 0; // Inicializamos el contador en 0
+
+  void _incrementCounter() {
+    setState(() {
+      counter++; // Incrementamos el contador
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hola Mundo App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$counter', // Mostramos el contador
+              style: const TextStyle(fontSize: 48),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _incrementCounter, // Incrementar el contador al presionar
+              child: const Text('Incrementar'),
+            ),
+          ],
         ),
       ),
     );
